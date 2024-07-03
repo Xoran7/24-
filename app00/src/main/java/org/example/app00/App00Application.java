098bf53b9b2941eeeb92aee72dc4fa9e255a04d7
xR7 <xr66nb@163.com>
2024-07-03 15:16:21 +0800

    配置pom环境, 并让idea配置本地Maven

---
+++ app00/src/main/java/org/example/app00/App00Application.java
@@ -0,0 +1,13 @@
+package org.example.app00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App00Application.class, args);
+    }
+
+}
